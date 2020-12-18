28b2275ae0ad4de742ac50454090870fa7a3e421
Evan Cameron <ecameron@bluecatnetworks.com>
2020-12-18 00:51:40 -0500

    day 11 p1

---
+++ src/day11.rs
@@ -0,0 +1,109 @@
+use anyhow::Result;
+use std::{convert::TryFrom, ops::Deref};
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Seat {
+    Floor = b'.',
+    Occupied = b'#',
+    Empty = b'L',
+}
+
+impl From<u8> for Seat {
+    fn from(value: u8) -> Self {
+        match value {
+            b'.' => Seat::Floor,
+            b'#' => Seat::Occupied,
+            b'L' => Seat::Empty,
+            _ => unreachable!("seat type not allowed"),
+        }
+    }
+}
+
+#[rustfmt::skip]
+const DIRS: [(isize, isize); 8] = [
+    (-1, -1), (0, -1), (1, -1), 
+    (-1,  0),          (1,  0),
+    (-1,  1), (0,  1), (1,  1)
+];
+
+pub fn compute() -> Option<usize> {
+    let mut board = include_str!("../input/day11.txt")
+        .lines()
+        .map(|line| line.bytes().map(Seat::from).collect())
+        .collect::<Vec<Vec<Seat>>>();
+
+    Sim { board }.run()
+}
+
+struct Sim {
+    board: Vec<Vec<Seat>>,
+}
+
+impl Sim {
+    fn new(board: Vec<Vec<Seat>>) -> Self {
+        Self { board }
+    }
+
+    fn run(&mut self) -> Option<usize> {
+        while self.step()? {
+            // println!("{:#?}", self.board);
+        }
+        Some(self.count_occupied())
+    }
+
+    fn step(&mut self) -> Option<bool> {
+        let rows = self.board.len();
+        let cols = self.board.first()?.len();
+        let mut new_board = self.board.clone();
+        let mut changed = false;
+
+        for y in 0..rows {
+            for x in 0..cols {
+                let occupied = self
+                    .get_adj(x, y)
+                    .filter(|&seat| seat == Seat::Occupied)
+                    .count();
+                match new_board[y][x] {
+                    Seat::Empty if occupied == 0 => {
+                        new_board[y][x] = Seat::Occupied;
+                        changed = true;
+                    }
+                    Seat::Occupied if occupied >= 4 => {
+                        new_board[y][x] = Seat::Empty;
+                        changed = true;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        if changed {
+            self.board = new_board;
+        }
+
+        Some(changed)
+    }
+
+    fn count_occupied(&self) -> usize {
+        self.board
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .copied()
+                    .filter(|&seat| seat == Seat::Occupied)
+                    .count()
+            })
+            .sum()
+    }
+
+    fn get_adj(&self, x: usize, y: usize) -> impl Iterator<Item = Seat> + '_ {
+        DIRS.iter().copied().flat_map(move |(dx, dy)| {
+            Some(
+                *self
+                    .board
+                    .get((y as isize + dy) as usize)?
+                    .get((x as isize + dx) as usize)?,
+            )
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 mod day1;
 mod day10;
+mod day11;
 mod day2;
 mod day3;
 mod day4;
@@ -21,5 +22,6 @@ fn main() {
     // day7::compute();
     // println!("{}", day8::part_two());
     // println!("{:?}", day9::part_two_fast());
-    println!("{:?}", day10::part_two_naive());
+    // println!("{:?}", day10::part_two_naive());
+    println!("{:?}", day11::compute());
 }
