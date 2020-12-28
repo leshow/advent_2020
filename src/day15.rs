30044d3f02490558d3d1d268e589c0ffa0583d01
Evan Cameron <cameron.evan@gmail.com>
2020-12-28 02:10:12 -0500

    day 15p2

--- src/day15.rs
+++ src/day15.rs
@@ -18,3 +18,40 @@ pub fn part_one() -> Option<usize> {
 
     stack.pop()
 }
+
+pub fn part_two() -> Option<usize> {
+    use std::collections::{hash_map::Entry, HashMap};
+
+    let vals = [1, 20, 11, 6, 12, 0];
+    let mut map = vals
+        .iter()
+        .enumerate()
+        .map(|(i, v)| (*v, (None, i + 1)))
+        .collect::<HashMap<_, _>>();
+    let mut prev = *vals.last()?;
+
+    for round in vals.len() + 1..=30_000_000 {
+        prev = match map.entry(prev) {
+            Entry::Occupied(entry) => match *entry.get() {
+                (None, _) => 0,
+                (Some(before), last) => last - before,
+            },
+            Entry::Vacant(_) => 0,
+        };
+
+        match map.entry(prev) {
+            Entry::Vacant(entry) => {
+                entry.insert((None, round));
+            }
+            Entry::Occupied(mut entry) => match *entry.get() {
+                (None, last_seen) => {
+                    entry.insert((Some(last_seen), round));
+                }
+                (Some(_), last_seen) => {
+                    entry.insert((Some(last_seen), round));
+                }
+            },
+        }
+    }
+    Some(prev)
+}
